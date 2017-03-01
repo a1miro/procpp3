c9dc019a75300d28670716c2a233c4eb048d82c0
Andrei Mironenko <andrei.mironenko@gmail.com>
2017-03-01 17:56:22 +0000

    Adding some more std::string examples

--- chapter02/cppstrings.cpp
+++ chapter02/cppstrings.cpp
@@ -12,12 +12,27 @@ int main()
     cout << "len(string1)=" << strlen(string1) << endl;
     cout << "len(string2)=" << string2.length() << endl;
 
-    
+    //Example of using std::string for number conversation
+    string orbits(to_string(686.97));
+    orbits += " "; //separation symbol
+    orbits += to_string(365.24);
+
+    cout << "orbits: " << orbits << endl;
 
 
+    //string orbits ("686.97 365.24");
+    string::size_type sz;     // alias of size_t
 
+    float mars = stof (orbits,&sz);
+    float earth = stof (orbits.substr(sz));
+    std::cout << "One martian year takes " << (mars/earth) << " Earth years.\n";
 
-
+    // Raw strings literal examples
+    string greetings = R"(Hello "World"!)";
+    string multiline = R"(Line 1
+                            Line2 with \t)";
+    cout << "greetings: " << greetings << endl;
+    cout << "multiline: " << multiline << endl;
 
     return 0;
 }
