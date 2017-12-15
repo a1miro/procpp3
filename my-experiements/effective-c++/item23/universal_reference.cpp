6b25dcd497a2b50abb4f2bac71dbd667f1968158
Andrei Mironenko <andrei.mironenko@imgtec.com>
2017-12-15 18:33:11 +0000

    Adding profiling to the item23

--- my-experiements/effective-c++/item23/universal_reference.cpp
+++ my-experiements/effective-c++/item23/universal_reference.cpp
@@ -6,15 +6,15 @@ using namespace std;
 using namespace std::chrono;
 
 auto profile = [](auto&& func, auto&&... params) {
-    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    //high_resolution_clock::time_point t1 = high_resolution_clock::now();
 
     std::forward<decltype(func)>(func)(
             std::forward<decltype(params)>(params)...
             );
 
-    high_resolution_clock::time_point t2 = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>( t2 - t1 ).count();
-    cout << "duration is " << duration << endl;
+    //high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    //auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+    //cout << "duration is " << duration << endl;
 };
 
 void factorial(uint32_t n) {
@@ -23,13 +23,13 @@ void factorial(uint32_t n) {
         s *= i;
     }
 
-    cout << "factorial of " << n << " is equal " << s << endl;
+    //cout << "factorial of " << n << " is equal " << s << endl;
 }
 
 int main(int argc, char** argv) 
 {
     using namespace std;
-
+#if 0
     int var1 = 6;
     auto&& var2 = var1;
 
@@ -49,7 +49,26 @@ int main(int argc, char** argv)
     profile(factorial, n);
     n=20;
     profile(factorial, n);
-    
+#endif
+
+    cout << "================== Direct call to the function ===============================" << endl;
+    high_resolution_clock::time_point t1 = high_resolution_clock::now();
+    for (int i = 0; i < 1000000; i ++) {
+        factorial(5);
+    }
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>( t2 - t1 ).count();
+    cout << " takes this duration : " << duration << endl;
+
+    cout << "================== Call to the function template ===============================" << endl;
+    t1 = high_resolution_clock::now();
+    for (int i = 0; i < 1000000; i ++) {
+        profile(factorial,5);
+    }
+    t2 = high_resolution_clock::now();
+    duration = duration_cast<microseconds>( t2 - t1 ).count();
+    cout << " takes this duration : " << duration << endl;
+
 
     return 0;
 }
