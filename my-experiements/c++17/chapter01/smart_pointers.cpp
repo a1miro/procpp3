082c0aa40b3603f932381d78defbc094e2e19fe0
Andrei Mironenko <andrei.mironenko@paralleldynamic.com>
2018-06-17 23:30:33 +0100

    Adding smartpointers example in chapter01

--- my-experiements/c++17/chapter01/smart_pointers.cpp
+++ my-experiements/c++17/chapter01/smart_pointers.cpp
@@ -1,6 +1,42 @@
 #include<iostream>
+#include<memory>
+
+
 
 int main() {
+    using namespace std;
+
+    const size_t ints_num = 10, floats_num = ints_num;
+
+    auto ints = make_unique<int[]>(ints_num);
+    auto floats = make_unique<float[]>(floats_num);
+
+    for (int i = 0; i < ints_num; i++) 
+    {
+        ints[i] = i;
+        floats[i] = ints[i] * 0.1;
+    }
+
+    cout << "ints:" << endl;
+    for (int i = 0; i < ints_num; i++) 
+    {
+        cout << ints[i] << ", ";
+    }
+    cout << endl;
+
+    cout << "floats:" << endl;
+    for (int i = 0; i < ints_num; i++) 
+    {
+        cout << floats[i] << ", ";
+    }
+    cout << endl;
+
+
+
+
+
+
+    
 
     return 0;
 }
