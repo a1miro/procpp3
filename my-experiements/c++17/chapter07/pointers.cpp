95ffac384dde084df5b16c755df60c79ff60b9b4
Andrei Mironenko <andrei.mironenko@gmail.com>
2019-02-21 22:25:29 +0000

    Adding pointers example

---
+++ my-experiements/c++17/chapter07/pointers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#define TP()    cout << __FILE__ << " : " << __FUNCTION__ << " : " << __LINE__ << endl
+#define WONT_COMPILE
+
+int main() {
+    TP();
+#if defined(WONT_COMPILE)
+    int* a = static_cast<int*>(new char("Hello world"));
+    long int* b = static_cast<long int*>(new int(3));
+#endif // WONT_COMPILE
+
+    return 0;
+}
