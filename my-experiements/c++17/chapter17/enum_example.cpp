1ad9d78703ab30d7e8aaae756809b9c75db41535
Andrei Mironenko <andrei.mironenko@gmail.com>
2020-12-21 00:24:03 +0000

    Adding time and duration examples

---
+++ my-experiements/c++17/chapter17/enum_example.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+enum class cmd_id_t : int {
+  CMD_STATUS = 0,
+  CMD_STOP
+};
+
+enum class cmd_simple_id_t : cmd_id_t {
+  CMD_ECHO = CMD_STOP + 1
+};
+
+int main() {
+
+  for (int id = cmd_id_t.CMD_STATUS, 
+      id <=  cmd_simple_id_t::CMD_ECHO; id++)
+  {
+    cmd_simple_id = static_cast<cmd_dimple_id_t>(id) ;
+    swich(cmd_simple_id) {
+      case CMD_STATUS:
+        std::cout << "CMD_STATUS" << std::endl;
+        break;
+      case CMD_STOP:
+        std::cout << "CMD_STOP" << std::endl;
+        break;
+      case CMD_ECHO:
+        std::cout << "CMD_ECHO" << std::endl;
+        break;
+      default:
+        std::cout << "Unkown command" << std::endl;
+    }
+  }
+  return 0;
+}
